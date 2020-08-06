9d2a2e1db19d21436659c6f32ccb2dd31fabfaca
SwetaNikki <56434154+SwetaNikki@users.noreply.github.com>
2020-08-06 13:08:51 +0530

    Create MakingSentences.py
    Implement a Python program to generate all sentences where subject is in
    ["Americans", "Indians"] and verb is in ["Play", "watch"] and the object is in
    ["Baseball","cricket"].

---
+++ MakingSentences.py
@@ -0,0 +1,15 @@
+"""
+2. Implement a Python program to generate all sentences where subject is in
+["Americans", "Indians"] and verb is in ["Play", "watch"] and the object is in
+["Baseball","cricket"].
+
+"""
+
+subject = ['Americans', 'Indians']
+verb = ['Play', 'watch']
+objects = ['Baseball','cricket']
+
+for sub in subject :
+    for v in verb:
+        for ob in objects:
+            print(sub+" "+v+" "+ob+".")
